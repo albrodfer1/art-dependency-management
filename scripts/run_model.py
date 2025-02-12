bb4532293ac43967febe9b981d2dcaee9d5f7ace
Alberto Rodriguez <alberto.rfernandez4@gmail.com>
2025-02-12 17:56:34 +0100

    update script

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -1,4 +1,4 @@
-from transformers import VivitForVideoClassification, AutoImageProcessor
+from transformers import VivitModel, AutoImageProcessor
 import torch
 import torchvision.transforms as transforms
 import cv2
@@ -6,7 +6,7 @@ import numpy as np
 
 # Load the model and processor
 model_id = "google/vivit-b-16x2-kinetics400"  # Pretrained on Kinetics-400 dataset
-model = VivitForVideoClassification.from_pretrained(model_id)
+model = VivitModel.from_pretrained(model_id)
 processor = AutoImageProcessor.from_pretrained(model_id)
 
 def load_video(video_path, num_frames=16, frame_size=(224, 224)):
